0c4edca65107bb6b759a85506cd27758d291d2f2
rodrigomha <rodrigomha@gmail.com>
2024-06-20 13:58:56 -0700

    update portfolio test

--- scripts/portfolio_test.jl
+++ scripts/portfolio_test.jl
@@ -1,9 +1,14 @@
-#data = PSY._create_system_data_from_kwargs()
-
-#bus = ACBus(nothing)
-#discount_rate = 0.07
-#investment_schedule = Dict()
-#port_metadata = PSIP.PortfolioMetadata("portfolio_test", nothing, nothing)
+using Revise
+import InfrastructureSystems
+using PowerSystemsInvestmentsPortfolios
+using PowerSystemsInvestments
+using PowerSystems
+using JuMP
+using DataStructures
+const IS = InfrastructureSystems
+const PSIP = PowerSystemsInvestmentsPortfolios
+const PSY = PowerSystems
+const PSIN = PowerSystemsInvestments
 
 p = Portfolio(0.07)
 
@@ -11,7 +16,7 @@ t_th = SupplyTechnology{ThermalStandard}(;
     base_power=100.0,
     prime_mover_type=PrimeMovers.ST,
     capital_cost=LinearFunctionData(50.0),
-    minimum_required_capacity=0.0,    
+    minimum_required_capacity=0.0,
     gen_ID="1",
     available=true,
     name="thermal_tech",
@@ -41,27 +46,27 @@ t_th_ext["operational_periods_2"] = [
     6,  # second investment period
 ]
 t_th_ext["investment_operational_periods_map"] =
-    Dict(1 => 2030, 2 => 2030, 3 => 2030, 4 => 2040, 5 => 2040, 6 => 2040)
+    OrderedDict(1 => 2030, 2 => 2030, 3 => 2030, 4 => 2040, 5 => 2040, 6 => 2040)
 t_th_ext["initial_capacity"] = 200.0 # Data in MW
 t_th_ext["maximum_capacity"] = 10000.0
 t_th_ext["minimum_required_capacity"] = [0.0, 0.0]
 
 t_th_exp = SupplyTechnology{ThermalStandard}(;
     base_power=100.0,
-    capital_cost=150.0,
+    prime_mover_type=PrimeMovers.ST,
+    capital_cost=LinearFunctionData(150.0),
     minimum_required_capacity=0.0,
-    prime_mover_type="ST",
-    available=true,
     gen_ID="1",
+    available=true,
     name="thermal_tech_expensive",
     initial_capacity=200.0,
-    fuel="WASTE_OIL",
+    fuel=ThermalFuels.WASTE_OIL,
     power_systems_type="ThermalStandard",
-    variable_cost=1.0,
+    variable_cost=LinearFunctionData(1.0),
     balancing_topology="Region",
-    operations_cost=15.0,
+    operations_cost=LinearFunctionData(15.0),
     maximum_capacity=10000.0,
-    capacity_factor="0.92",
+    capacity_factor=0.92,
 )
 
 t_th_exp_ext = t_th_exp.ext
@@ -81,29 +86,29 @@ t_th_exp_ext["operational_periods_2"] = [
 ]
 
 t_th_exp_ext["investment_operational_periods_map"] =
-    Dict(1 => 2030, 2 => 2030, 3 => 2030, 4 => 2040, 5 => 2040, 6 => 2040)
+    OrderedDict(1 => 2030, 2 => 2030, 3 => 2030, 4 => 2040, 5 => 2040, 6 => 2040)
 t_th_exp_ext["initial_capacity"] = 200.0 # Data in MW
 t_th_exp_ext["maximum_capacity"] = 10000.0
 t_th_exp_ext["minimum_required_capacity"] = [0.0, 0.0]
 
 # Renewable Technology
 
-t_re = SupplyTechnology{ThermalStandard}(;
+t_re = SupplyTechnology{RenewableDispatch}(;
     base_power=100.0,
-    capital_cost=100.0,
+    prime_mover_type=PrimeMovers.WT,
+    capital_cost=LinearFunctionData(100.0),
     minimum_required_capacity=0.0,
-    prime_mover_type="WT",
-    available=true,
     gen_ID="1",
+    available=true,
     name="renewable_tech",
     initial_capacity=200.0,
-    fuel="OTHER",
-    power_systems_type="ThermalStandard",
-    variable_cost=0.0,
+    fuel=ThermalFuels.OTHER,
+    power_systems_type="RenewableDispatch",
+    variable_cost=LinearFunctionData(0.0),
     balancing_topology="Region",
-    operations_cost=10.0,
+    operations_cost=LinearFunctionData(10.0),
     maximum_capacity=10000.0,
-    capacity_factor="1.0",
+    capacity_factor=1.0, # There is a variable cap
 )
 
 t_re_ext = t_re.ext
@@ -123,24 +128,41 @@ t_re_ext["operational_periods_2"] = [
     6,  # second investment period
 ]
 t_re_ext["investment_operational_periods_map"] =
-    Dict(1 => 2030, 2 => 2030, 3 => 2030, 4 => 2040, 5 => 2040, 6 => 2040)
+    OrderedDict(1 => 2030, 2 => 2030, 3 => 2030, 4 => 2040, 5 => 2040, 6 => 2040)
 t_re_ext["initial_capacity"] = 200.0 # Data in MW
 t_re_ext["maximum_capacity"] = 10000.0
 t_re_ext["minimum_required_capacity"] = [0.0, 0.0]
 
 # Demand side technologies
-d_1 = PSIP.DemandRequirement{ElectricLoad}(
+d_1 = DemandRequirement{ElectricLoad}(
     load_growth=0.05,
     name="demand",
     available=true,
     power_systems_type="ElectricLoad",
     region="1",
-    peak_load=500.0,
+    peak_load=100.0,
 )
 
+d_1_ext = d_1.ext
+d_1_ext["load_scale_factor"] =
+    OrderedDict(1 => 1.0, 2 => 1.5, 3 => 1.75, 4 => 0.8, 5 => 3, 6 => 1.2)
+
+temp_container = Dict()
+temp_container["components"] =
+    Dict(t_th.name => t_th, t_th_exp.name => t_th_exp, t_re.name => t_re, d_1.name => d_1)
+temp_container["data"] = Dict()
+temp_container["data"]["investment_operational_periods_map"] =
+    OrderedDict(1 => 2030, 2 => 2030, 3 => 2030, 4 => 2040, 5 => 2040, 6 => 2040)
+temp_container["data"]["investment_periods"] = [2030, 2040]
+temp_container["variables"] = Dict()
+temp_container["expressions"] = Dict()
+temp_container["constraints"] = Dict()
+
+#=
 PSIP.add_technology!(p, t_th)
 PSIP.add_technology!(p, t_re)
 PSIP.add_technology!(p, t_th_exp)
+PSIP.add_technology!(p, d_1)
 
 IS.serialize(t_th)
 IS.serialize(t_re)
@@ -156,3 +178,4 @@ IS.serialize(p)
 #get_available(t_re)
 #get_available(t_th_exp)
 #IS.deserialize(p)
+=#
