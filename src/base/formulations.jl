20633b89b7f64c0cd8eb3b47c74da97f6852910d
rodrigomha <rodrigomha@gmail.com>
2024-06-26 12:24:38 -0700

    add new formulations

--- src/base/formulations.jl
+++ src/base/formulations.jl
@@ -1,8 +1,19 @@
 abstract type InvestmentTechnologyFormulation end
 abstract type OperationsTechnologyFormulation end
 
+
+### Investment Formulations ###
+
 struct ContinuousInvestment <: InvestmentTechnologyFormulation end
 struct IntegerInvestment <: InvestmentTechnologyFormulation end
 
+### Operations Formulations ###
+
 struct BasicDispatch <: OperationsTechnologyFormulation end
-struct NoDispatch <: OperationsTechnologyFormulation end
+struct ThermalNoDispatch <: OperationsTechnologyFormulation end
+struct RenewableNoDispatch <: OperationsTechnologyFormulation end
+
+"""
+Infinite capacity approximation of network flow to represent entire system with a single node.
+"""
+struct CopperPlatePowerModel <: PM.AbstractActivePowerModel end
