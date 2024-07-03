cc0389f539f52408a4e3d05cb3a058cc61529551
rodrigomha <rodrigomha@gmail.com>
2024-07-03 10:25:10 -0700

    add investment model

--- src/base/investment_model.jl
+++
@@ -1,7 +0,0 @@
-mutable struct InvestmentModel{I <: InvestmentProblem, S <: SolutionAlgorithm}
-    name::Symbol
-    portfolio::PSIP.Portfolio
-    internal::Union{Nothing, ISOPT.ModelInternal}
-    store::InvestmentModelStore
-    ext::Dict{String, Any}
-end
