c1ab161452ca649a709841bce42a31a507e764dd
Jose Daniel Lara <jdlara@berkeley.edu>
2024-07-03 13:01:18 -0600

    rename expression

--- src/base/expressions.jl
+++ src/base/expressions.jl
@@ -3,7 +3,7 @@ struct DemandTotal <: ISOPT.ExpressionType end
 
 struct CumulativeCapacity <: ISOPT.ExpressionType end
 struct CapitalCost <: ISOPT.ExpressionType end
-struct FixedOMCost <: ISOPT.ExpressionType end
+struct FixedOperationModelCost <: ISOPT.ExpressionType end
 
 struct TotalCapitalCost <: ISOPT.ExpressionType end
 
