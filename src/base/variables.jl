6fec24af5db32be4d35b405d52abe36270a35147
rodrigomha <rodrigomha@gmail.com>
2024-08-12 21:41:39 -0700

    add power variable

--- src/base/variables.jl
+++ src/base/variables.jl
@@ -3,7 +3,16 @@ abstract type SparseVariableType <: ISOPT.VariableType end
 abstract type InvestmentVariableType <: ISOPT.VariableType end
 abstract type OperationsVariableType <: ISOPT.VariableType end
 
+### Investment Variables ###
+
 """
 Total installed capacity for a technology
 """
 struct BuildCapacity <: InvestmentVariableType end
+
+### Operations Variables ###
+
+"""
+Total installed capacity for a technology
+"""
+struct ActivePowerVariable <: OperationsVariableType end
