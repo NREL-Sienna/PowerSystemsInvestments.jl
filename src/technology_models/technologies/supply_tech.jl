acfa3e4280be2eb16f584416e582b2eeb7dba9d2
Jerry Potts <jerry.potts@utexas.edu>
2024-08-14 17:10:54 -0600

    add capacity expression and max capacity constraint

--- src/technology_models/technologies/supply_tech.jl
+++ src/technology_models/technologies/supply_tech.jl
@@ -33,6 +33,48 @@ end
 
 ################### Variables ####################
 
+################## Expressions ###################
+
+function add_expression!(
+    container::SingleOptimizationContainer,
+    expression_type::T,
+    devices::U,
+    formulation::AbstractTechnologyFormulation,
+) where {
+    T <: InvestmentExpressionType,
+    U <: Union{Vector{D}, IS.FlattenIteratorWrapper{D}},
+} where {D <: PSIP.Technology}
+    @assert !isempty(devices)
+    time_steps = get_time_steps_investments(container)
+    binary = false
+
+    var = get_variable(container, BuildCapacity(), D)
+
+    expression = add_expression_container!(
+        container,
+        expression_type,
+        D,
+        [PSIP.get_name(d) for d in devices],
+        time_steps,
+    )
+
+    for t in time_steps, d in devices
+        name = PSIP.get_name(d)
+        expression[name, t] = JuMP.@expression(
+            get_jump_model(container),
+            sum(var[name,t_p] for t_p in time_steps if t_p <= t),
+            #binary = binary
+        )
+        #ub = get_variable_upper_bound(expression_type, d, formulation)
+        #ub !== nothing && JuMP.set_upper_bound(variable[name, t], ub)
+
+        #lb = get_variable_lower_bound(expression_type, d, formulation)
+        #lb !== nothing && JuMP.set_lower_bound(variable[name, t], lb)
+    end
+
+    return
+end
+
 ################### Constraints ##################
 
 function add_constraints!(
@@ -83,6 +125,90 @@ function add_constraints!(
                     ts_data[ix] * installed_cap[name, time_step_inv]
                 )
             end
+        end
+    end
+end
+
+#Essentially the same constraint as above, just removed the variable capacity factor since not needed
+#for thermal gen
+function add_constraints!(
+    container::SingleOptimizationContainer,
+    ::Type{T},
+    ::V,
+    devices::U,
+    model,
+    ::NetworkModel{X},
+) where {
+    T <: ActivePowerLimitsConstraint,
+    U <: Union{Vector{D}, IS.FlattenIteratorWrapper{D}},
+    V <: ActivePowerVariable,
+    X <: PM.AbstractPowerModel,
+} where {D <: PSIP.SupplyTechnology{PSY.ThermalStandard}}
+    time_steps = get_time_steps(container)
+    # Hard Code Mapping #
+    # TODO: Remove
+    @warn("creating hard code mapping. Remove it later")
+    mapping_ops = Dict(("2030", 1) => 1:24, ("2035", 1) => 25:48)
+    mapping_inv = Dict("2030" => 1, "2035" => 2)
+    device_names = PSIP.get_name.(devices)
+    con_ub = add_constraints_container!(container, T(), D, device_names, time_steps)
+
+    @warn("We should use the expression TotalCapacity rather than BuildCapacity")
+    installed_cap = get_variable(container, BuildCapacity(), D)
+    active_power = get_variable(container, V(), D)
+
+    for d in devices
+        name = PSIP.get_name(d)
+        ts_name = "ops_variable_cap_factor"
+        ts_keys = filter(x -> x.name == ts_name, IS.get_time_series_keys(d))
+        for ts_key in ts_keys
+            features = ts_key.features
+            year = features["year"]
+            rep_day = features["rep_day"]
+            time_steps_ix = mapping_ops[(year, rep_day)]
+            time_step_inv = mapping_inv[year]
+
+            for t in time_steps_ix
+                con_ub[name, t] = JuMP.@constraint(
+                    get_jump_model(container),
+                    active_power[name, t] <=
+                    installed_cap[name, time_step_inv]
+                )
+            end
+        end
+    end
+end
+
+# Maximum cumulative capacity
+function add_constraints!(
+    container::SingleOptimizationContainer,
+    ::Type{T},
+    ::V,
+    devices::U,
+    model,
+    #::NetworkModel{X},
+) where {
+    T <: MaximumCumulativeCapacity,
+    U <: Union{Vector{D}, IS.FlattenIteratorWrapper{D}},
+    V <: CumulativeCapacity,
+    #X <: PM.AbstractPowerModel,
+} where {D <: PSIP.SupplyTechnology}
+    time_steps = get_time_steps_investments(container)
+
+    device_names = PSIP.get_name.(devices)
+    con_ub = add_constraints_container!(container, T(), D, device_names, time_steps)
+
+    installed_cap = get_expression(container, CumulativeCapacity(), D)
+
+    for d in devices
+        name = PSIP.get_name(d)
+        max_capacity = PSIP.get_maximum_capacity(d)
+        for t in time_steps
+            con_ub[name, t] = JuMP.@constraint(
+                    get_jump_model(container),
+                    installed_cap[name, t] <=
+                    max_capacity
+                )
         end
     end
 end
