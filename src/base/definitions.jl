1a2d52f0a30fa590b443189f7e396afe2a95115a
rodrigomha <rodrigomha@gmail.com>
2024-08-22 10:30:16 -0700

    add SOS enum

--- src/base/definitions.jl
+++ src/base/definitions.jl
@@ -1,3 +1,5 @@
 const UNSET_HORIZON = Dates.Millisecond(0)
 const UNSET_RESOLUTION = Dates.Millisecond(0)
 const UNSET_INI_TIME = Dates.DateTime(0)
+
+IS.@scoped_enum(SOSStatusVariable, NO_VARIABLE = 1, PARAMETER = 2, VARIABLE = 3,)
