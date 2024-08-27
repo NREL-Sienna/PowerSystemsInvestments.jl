3d761df64b592281551f25b8913b8ccc0b1b71fb
Jerry Potts <jerry.potts@utexas.edu>
2024-08-26 18:41:33 -0600

    add power balance constraint

--- src/technology_models/technologies/demand_tech.jl
+++ src/technology_models/technologies/demand_tech.jl
@@ -87,100 +87,25 @@ end
 function add_constraints!(
     container::SingleOptimizationContainer,
     ::Type{T},
-    ::V,
-    devices::U,
-    model,
-    ::NetworkModel{X},
+    ::Type{U},
+    #model,
+    #::NetworkModel{X},
 ) where {
-    T <: ActivePowerVariableLimitsConstraint,
-    U <: Union{Vector{D}, IS.FlattenIteratorWrapper{D}},
-    V <: ActivePowerVariable,
-    X <: PM.AbstractPowerModel,
-} where {D <: PSIP.SupplyTechnology{PSY.RenewableDispatch}}
+    T <: SupplyDemandBalance,
+    U <: PSIP.DemandRequirement{PSY.PowerLoad},
+    #X <: PM.AbstractPowerModel,
+} 
+    # TODO: Remove technologies from the expression definition for these and add corresponding get_expression functions
     time_steps = get_time_steps(container)
-    # Hard Code Mapping #
-    # TODO: Remove
-    @warn("creating hard code mapping. Remove it later")
-    mapping_ops = Dict(("2030", 1) => 1:24, ("2035", 1) => 25:48)
-    mapping_inv = Dict("2030" => 1, "2035" => 2)
-    device_names = PSIP.get_name.(devices)
-    con_ub = add_constraints_container!(container, T(), D, device_names, time_steps)
 
-    @warn("We should use the expression TotalCapacity rather than BuildCapacity")
-    installed_cap = get_variable(container, BuildCapacity(), D)
-    active_power = get_variable(container, V(), D)
+    energy_balance = add_constraints_container!(container, T(), U, time_steps)
 
-    for d in devices
-        name = PSIP.get_name(d)
-        ts_name = "ops_variable_cap_factor"
-        ts_keys = filter(x -> x.name == ts_name, IS.get_time_series_keys(d))
-        for ts_key in ts_keys
-            ts_type = ts_key.time_series_type
-            features = ts_key.features
-            year = features["year"]
-            rep_day = features["rep_day"]
-            ts_data = TimeSeries.values(
-                IS.get_time_series(ts_type, d, ts_name; year=year, rep_day=rep_day).data,
-            )
-            time_steps_ix = mapping_ops[(year, rep_day)]
-            time_step_inv = mapping_inv[year]
-
-            for (ix, t) in enumerate(time_steps_ix)
-                con_ub[name, t] = JuMP.@constraint(
+    for t in time_steps
+        supply = get_expression(container, SupplyTotal(), PSIP.SupplyTechnology{PSY.RenewableDispatch})
+        demand = get_expression(container, DemandTotal(), PSIP.DemandRequirement{PSY.PowerLoad})
+        energy_balance[t] = JuMP.@constraint(
                     get_jump_model(container),
-                    active_power[name, t] <=
-                    ts_data[ix] * installed_cap[name, time_step_inv]
+                    supply - demand >= 0
                 )
-            end
-        end
-    end
-end
-
-#Essentially the same constraint as above, just removed the variable capacity factor since not needed
-#for thermal gen
-function add_constraints!(
-    container::SingleOptimizationContainer,
-    ::Type{T},
-    ::V,
-    devices::U,
-    model,
-    ::NetworkModel{X},
-) where {
-    T <: ActivePowerLimitsConstraint,
-    U <: Union{Vector{D}, IS.FlattenIteratorWrapper{D}},
-    V <: ActivePowerVariable,
-    X <: PM.AbstractPowerModel,
-} where {D <: PSIP.SupplyTechnology{PSY.ThermalStandard}}
-    time_steps = get_time_steps(container)
-    # Hard Code Mapping #
-    # TODO: Remove
-    @warn("creating hard code mapping. Remove it later")
-    mapping_ops = Dict(("2030", 1) => 1:24, ("2035", 1) => 25:48)
-    mapping_inv = Dict("2030" => 1, "2035" => 2)
-    device_names = PSIP.get_name.(devices)
-    con_ub = add_constraints_container!(container, T(), D, device_names, time_steps)
-
-    @warn("We should use the expression TotalCapacity rather than BuildCapacity")
-    installed_cap = get_variable(container, BuildCapacity(), D)
-    active_power = get_variable(container, V(), D)
-
-    for d in devices
-        name = PSIP.get_name(d)
-        ts_name = "ops_variable_cap_factor"
-        ts_keys = filter(x -> x.name == ts_name, IS.get_time_series_keys(d))
-        for ts_key in ts_keys
-            features = ts_key.features
-            year = features["year"]
-            rep_day = features["rep_day"]
-            time_steps_ix = mapping_ops[(year, rep_day)]
-            time_step_inv = mapping_inv[year]
-
-            for t in time_steps_ix
-                con_ub[name, t] = JuMP.@constraint(
-                    get_jump_model(container),
-                    active_power[name, t] <= installed_cap[name, time_step_inv]
-                )
-            end
-        end
     end
 end
