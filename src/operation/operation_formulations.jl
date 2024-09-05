8296862d5eeeb0d4b57632b82f00a6de3d162898
Jerry Potts <jerry.potts@utexas.edu>
2024-09-05 08:00:43 -0600

    typo

--- src/operation/operation_formulations.jl
+++ src/operation/operation_formulations.jl
@@ -7,4 +7,4 @@ struct RenewableNoDispatch <: OperationsTechnologyFormulation end
 """
 Infinite capacity approximation of network flow to represent entire system with a single node.
 """
-struct SingleRegionBalanceModel <: PM.AbstractActivePowerModel end
+struct SingleRegionPowerModel <: PM.AbstractActivePowerModel end
