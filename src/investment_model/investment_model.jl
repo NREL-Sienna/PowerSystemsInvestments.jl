6c6c684535576e617b735ab5364744147c571605
Jerry Potts <jerry.potts@utexas.edu>
2024-09-24 09:59:09 -0600

    add write_results!

--- src/investment_model/investment_model.jl
+++ src/investment_model/investment_model.jl
@@ -1,3 +1,8 @@
+# Aliases used for clarity in the method dispatches so it is possible to know if writing to
+# DecisionModel data or EmulationModel data
+const DecisionModelIndexType = Dates.DateTime
+const EmulationModelIndexType = Int
+
 mutable struct InvestmentModel{S <: SolutionAlgorithm}
     name::Symbol
     template::InvestmentModelTemplate
@@ -145,6 +150,217 @@ get_run_status(model::InvestmentModel) = get_run_status(get_simulation_info(mode
 set_run_status!(model::InvestmentModel, status) =
     set_run_status!(get_simulation_info(model), status)
 
+get_initial_time(model::InvestmentModel) = get_initial_time(get_settings(model))
+
+
+function write_results!(
+    store,
+    model::InvestmentModel,
+    index::Union{DecisionModelIndexType, EmulationModelIndexType},
+    update_timestamp::Dates.DateTime;
+    exports = nothing,
+)
+    if exports !== nothing
+        export_params = Dict{Symbol, Any}(
+            :exports => exports,
+            :exports_path => joinpath(exports.path, string(get_name(model))),
+            :file_type => get_export_file_type(exports),
+            :resolution => get_resolution(model),
+            :horizon_count => get_horizon(get_settings(model)) ÷ get_resolution(model),
+        )
+    else
+        export_params = nothing
+    end
+
+    write_model_dual_results!(store, model, index, update_timestamp, export_params)
+    write_model_parameter_results!(store, model, index, update_timestamp, export_params)
+    write_model_variable_results!(store, model, index, update_timestamp, export_params)
+    write_model_aux_variable_results!(store, model, index, update_timestamp, export_params)
+    write_model_expression_results!(store, model, index, update_timestamp, export_params)
+    return
+end
+
+function write_model_dual_results!(
+    store,
+    model::T,
+    index::Union{DecisionModelIndexType, EmulationModelIndexType},
+    update_timestamp::Dates.DateTime,
+    export_params::Union{Dict{Symbol, Any}, Nothing},
+) where {T <: InvestmentModel}
+    container = get_optimization_container(model)
+    model_name = get_name(model)
+    if export_params !== nothing
+        exports_path = joinpath(export_params[:exports_path], "duals")
+        mkpath(exports_path)
+    end
+
+    for (key, constraint) in get_duals(container)
+        !should_write_resulting_value(key) && continue
+        data = jump_value.(constraint)
+        write_result!(store, model_name, key, index, update_timestamp, data)
+
+        if export_params !== nothing &&
+           should_export_dual(export_params[:exports], index, model_name, key)
+            horizon_count = export_params[:horizon_count]
+            resolution = export_params[:resolution]
+            file_type = export_params[:file_type]
+            df = to_dataframe(jump_value.(constraint), key)
+            time_col = range(index; length = horizon_count, step = resolution)
+            DataFrames.insertcols!(df, 1, :DateTime => time_col)
+            IS.Optimization.export_result(file_type, exports_path, key, index, df)
+        end
+    end
+    return
+end
+
+function write_model_parameter_results!(
+    store,
+    model::T,
+    index::Union{DecisionModelIndexType, EmulationModelIndexType},
+    update_timestamp::Dates.DateTime,
+    export_params::Union{Dict{Symbol, Any}, Nothing},
+) where {T <: InvestmentModel}
+    container = get_optimization_container(model)
+    model_name = get_name(model)
+    if export_params !== nothing
+        exports_path = joinpath(export_params[:exports_path], "parameters")
+        mkpath(exports_path)
+    end
+
+    horizon = get_horizon(get_settings(model))
+    resolution = get_resolution(get_settings(model))
+    horizon_count = horizon ÷ resolution
+
+    parameters = get_parameters(container)
+    for (key, container) in parameters
+        !should_write_resulting_value(key) && continue
+        data = calculate_parameter_values(container)
+        write_result!(store, model_name, key, index, update_timestamp, data)
+
+        if export_params !== nothing &&
+           should_export_parameter(export_params[:exports], index, model_name, key)
+            resolution = export_params[:resolution]
+            file_type = export_params[:file_type]
+            df = to_dataframe(data, key)
+            time_col = range(index; length = horizon_count, step = resolution)
+            DataFrames.insertcols!(df, 1, :DateTime => time_col)
+            IS.Optimization.export_result(file_type, exports_path, key, index, df)
+        end
+    end
+    return
+end
+
+function write_model_variable_results!(
+    store,
+    model::T,
+    index::Union{DecisionModelIndexType, EmulationModelIndexType},
+    update_timestamp::Dates.DateTime,
+    export_params::Union{Dict{Symbol, Any}, Nothing},
+) where {T <: InvestmentModel}
+    container = get_optimization_container(model)
+    model_name = get_name(model)
+    if export_params !== nothing
+        exports_path = joinpath(export_params[:exports_path], "variables")
+        mkpath(exports_path)
+    end
+
+    if !isempty(container.primal_values_cache)
+        variables = container.primal_values_cache.variables_cache
+    else
+        variables = get_variables(container)
+    end
+
+    for (key, variable) in variables
+        !should_write_resulting_value(key) && continue
+        data = jump_value.(variable)
+        write_result!(store, model_name, key, index, update_timestamp, data)
+
+        if export_params !== nothing &&
+           should_export_variable(export_params[:exports], index, model_name, key)
+            horizon_count = export_params[:horizon_count]
+            resolution = export_params[:resolution]
+            file_type = export_params[:file_type]
+            df = to_dataframe(data, key)
+            time_col = range(index; length = horizon_count, step = resolution)
+            DataFrames.insertcols!(df, 1, :DateTime => time_col)
+            IS.Optimization.export_result(file_type, exports_path, key, index, df)
+        end
+    end
+    return
+end
+
+function write_model_aux_variable_results!(
+    store,
+    model::T,
+    index::Union{DecisionModelIndexType, EmulationModelIndexType},
+    update_timestamp::Dates.DateTime,
+    export_params::Union{Dict{Symbol, Any}, Nothing},
+) where {T <: InvestmentModel}
+    container = get_optimization_container(model)
+    model_name = get_name(model)
+    if export_params !== nothing
+        exports_path = joinpath(export_params[:exports_path], "aux_variables")
+        mkpath(exports_path)
+    end
+
+    for (key, variable) in get_aux_variables(container)
+        !should_write_resulting_value(key) && continue
+        data = jump_value.(variable)
+        write_result!(store, model_name, key, index, update_timestamp, data)
+
+        if export_params !== nothing &&
+           should_export_aux_variable(export_params[:exports], index, model_name, key)
+            horizon_count = export_params[:horizon_count]
+            resolution = export_params[:resolution]
+            file_type = export_params[:file_type]
+            df = to_dataframe(data, key)
+            time_col = range(index; length = horizon_count, step = resolution)
+            DataFrames.insertcols!(df, 1, :DateTime => time_col)
+            IS.Optimization.export_result(file_type, exports_path, key, index, df)
+        end
+    end
+    return
+end
+
+function write_model_expression_results!(
+    store,
+    model::T,
+    index::Union{DecisionModelIndexType, EmulationModelIndexType},
+    update_timestamp::Dates.DateTime,
+    export_params::Union{Dict{Symbol, Any}, Nothing},
+) where {T <: InvestmentModel}
+    container = get_optimization_container(model)
+    model_name = get_name(model)
+    if export_params !== nothing
+        exports_path = joinpath(export_params[:exports_path], "expressions")
+        mkpath(exports_path)
+    end
+
+    if !isempty(container.primal_values_cache)
+        expressions = container.primal_values_cache.expressions_cache
+    else
+        expressions = get_expressions(container)
+    end
+
+    for (key, expression) in expressions
+        !should_write_resulting_value(key) && continue
+        data = jump_value.(expression)
+        write_result!(store, model_name, key, index, update_timestamp, data)
+
+        if export_params !== nothing &&
+           should_export_expression(export_params[:exports], index, model_name, key)
+            horizon_count = export_params[:horizon_count]
+            resolution = export_params[:resolution]
+            file_type = export_params[:file_type]
+            df = to_dataframe(data, key)
+            time_col = range(index; length = horizon_count, step = resolution)
+            DataFrames.insertcols!(df, 1, :DateTime => time_col)
+            IS.Optimization.export_result(file_type, exports_path, key, index, df)
+        end
+    end
+    return
+end
+
 function init_model_store_params!(model::InvestmentModel)
     num_executions = get_executions(model)
     horizon = get_horizon(get_settings(model))
@@ -182,13 +398,12 @@ function build_pre_step!(model::InvestmentModel)
         end
         # Initial time are set here because the information is specified in the
         # Simulation Sequence object and not at the problem creation.
-        @warn "to-do: add init_optimization_container!"
-        #@info "Initializing Optimization Container For an InvestmentModel"
-        #init_optimization_container!(
-        #    get_optimization_container(model),
-        #    get_network_model(get_template(model)),
-        #    get_system(model),
-        #)
+        @info "Initializing Optimization Container For an InvestmentModel"
+        init_optimization_container!(
+            get_optimization_container(model),
+            get_transport_model(get_template(model)),
+            get_portfolio(model),
+        )
         @info "Initializing ModelStoreParams"
         init_model_store_params!(model)
         set_status!(model, ModelBuildStatus.IN_PROGRESS)
@@ -291,6 +506,7 @@ function solve!(
                         current_time,
                     )
                 end
+
                 if export_optimization_problem
                     TimerOutputs.@timeit RUN_OPERATION_MODEL_TIMER "Serialize" begin
                         serialize_problem(model; optimizer = optimizer)
@@ -305,7 +521,7 @@ function solve!(
                 end
                 @info "\n$(RUN_OPERATION_MODEL_TIMER)\n"
             catch e
-                @error "Decision Problem solve failed" exception = (e, catch_backtrace())
+                @error "Investment Problem solve failed" exception = (e, catch_backtrace())
                 set_run_status!(model, RunStatus.FAILED)
             end
         end
